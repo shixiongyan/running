9aa74d16c7f3b493a42611c2304ccf022c189c7e
YAN Shixiong <shixiongyan@foxmail.com>
2023-07-25 14:33:32 +0800

    更新文档
    我麻了，太烦了，搞了整天两了

---
+++ gatsby-config.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+module.exports = {
+  pathPrefix: process.env.PATH_PREFIX || '/',
+  siteMetadata: {
+    siteTitle: 'Bear的跑步记录',
+    siteUrl: 'https://shixiongyan.github.io/running/',
+    logo: 'https://profile-avatar.csdnimg.cn/095bbeeada4c48a28f9613711699e637_qq_40757240.jpg',
+    description: 'Personal site and blog',
+    navLinks: [
+      {
+        name: 'blibli',
+        url: 'https://space.bilibili.com/388110416',
+      },
+      {
+        name: 'About',
+        url: 'https://shixiongyan.github.io/running/',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+
+    {
+      resolve: 'gatsby-plugin-vercel',
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: '.',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+  ],
+};
